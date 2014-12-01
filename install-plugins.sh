34355fc2d262aebcde73ac5205308ab368e9c956
Takahiro Yano <Yano_Takahiro@ogis-ri.co.jp>
2014-12-01 12:54:41 +0000

    Add github and dependencies plugins

--- install-plugins.sh
+++ install-plugins.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
 
-mkdir -p /tmp/WEB-INF/plugins
-cd /tmp
+mkdir -p /tmp/joad-ci
+cd /tmp/joad-ci
+mkdir -p WEB-INF/plugins META-INF
 
 for plugin in \
-    git-client \
-    scm-api \
     git \
+    matrix-project \
+    scm-api \
+    credentials \
+    mailer \
+    ssh-credentials \
+    git-client \
+    github \
+    github-api \
     docker-build-step
 do
     curl -L https://updates.jenkins-ci.org/latest/${plugin}.hpi -o WEB-INF/plugins/${plugin}.hpi
 done
 
-zip -g /usr/share/jenkins/jenkins.war WEB-INF/plugins/* && rm -rf /tmp/WEB-INF
+unzip -p /usr/share/jenkins/jenkins.war META-INF/MANIFEST.MF \
+    | sed "/^\s*$/q" >META-INF/MANIFEST.MF
+
+zip -d /usr/share/jenkins/jenkins.war META-INF/JENKINS.SF
+zip -d /usr/share/jenkins/jenkins.war META-INF/JENKINS.RSA
+zip -g /usr/share/jenkins/jenkins.war META-INF/MANIFEST.MF
+zip -g /usr/share/jenkins/jenkins.war WEB-INF/plugins/*
+
+rm -rf /tmp/joad-ci
